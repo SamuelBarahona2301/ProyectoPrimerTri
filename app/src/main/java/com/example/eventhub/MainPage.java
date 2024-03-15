e8943dcb68d52bd208d817fae479c27161b02a90
Samuel <sbb232003@gmail.com>
2024-03-15 01:21:52 +0100

    hecho

--- app/src/main/java/com/example/eventhub/MainPage.java
+++ app/src/main/java/com/example/eventhub/MainPage.java
@@ -41,7 +41,7 @@ private ActivityMainPageBinding binding;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_profile)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_perfil)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_page);
